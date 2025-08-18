a723b06c56baba7dd8ab4575517532049da7046f
mfarhan <md254274@dal.ca>
2025-08-18 15:00:14 -0300

    Learned about root mean squared estimate

--- ml.py
+++ ml.py
@@ -2,7 +2,7 @@ from urllib.request import urlretrieve
 
 medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
 urlretrieve(medical_charges_url, 'medical.csv')
-
+import numpy as np
 import pandas as pd
 import jovian 
 import plotly.express as px
@@ -28,17 +28,30 @@ w= 50
 b =100
 
 
-def try_parameters(w,b):
-    ages = non_smoker_df.age 
-    target = non_smoker_df.charges
-    estimatedCharges = estimateCharges(ages,w,b)
+# def try_parameters(w,b):
+#     ages = non_smoker_df.age 
+#     target = non_smoker_df.charges
+#     estimatedCharges = estimateCharges(ages,w,b)
 
-    plt.plot(ages,estimatedCharges, 'r', alpha=0.9);
-    plt.scatter(ages,target,s=8,alpha=0.8);
-    plt.xlabel('Age');
-    plt.ylabel('Charges');
-    plt.legend(['Estimate' , 'Actual']);
-    plt.show()
+#     plt.plot(ages,estimatedCharges, 'r', alpha=0.9);
+#     plt.scatter(ages,target,s=8,alpha=0.8);
+#     plt.xlabel('Age');
+#     plt.ylabel('Charges');
+#     plt.legend(['Estimate' , 'Actual']);
+#     plt.show()
 
-try_parameters(60,200)
+# try_parameters(w,b)
+
+targets = non_smoker_df['charges']
+prediction = estimateCharges(non_smoker_df.age,w,b)
+
+def rmse(target, prediction):
+    return np.sqrt(np.mean(np.square(target-prediction)))
+print(rmse(targets,prediction))
+
+
+
+
+
+
 
